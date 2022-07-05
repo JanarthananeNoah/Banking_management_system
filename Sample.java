a8995b1d1d799f1634736a8711f7999140f58151
JanarthananeNoah <108712869+JanarthananeNoah@users.noreply.github.com>
2022-07-05 12:07:19 +0530

    initialize new variables a & b

--- Sample.java
+++ Sample.java
@@ -1,8 +1,7 @@
 class Sample{
     public static void main(String[] args) {
         System.out.println("Sample Program");
-    }
-    
-        
-    
+        int a = 10;
+        int b = 20;
+    } 
 }
